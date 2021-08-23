21014338c8e6d04281ba89071a3d06b73220dda5
Yara Kiryukhina <76944557+yarakiryukhina@users.noreply.github.com>
2021-08-23 12:11:45 +0100

    Add files via upload

--- globe.py
+++ globe.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import json
 
-sheet_id = 'Insert_sheet_id_here'
-sheet_name = 'Sheet1'
+sheet_id = '1lLDjY_I8PgjsKpWsQi125SRzESjFqUkDMsXehlwVy0E'
+url = 'https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}'
 
-url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-
-df = pd.read_csv(url, usecols=range(9))
+df = pd.read_csv(url.format(sheet_id, 'Sheet1'), usecols=range(10))
+un = pd.read_csv(url.format(sheet_id, 'Sheet2'), usecols=range(20))
 
 if __name__ == '__main__':
-    with open('output/food_waste.js', 'w', encoding='utf-8') as f:
-        f.write('var food_waste =\n' + json.dumps(df.to_dict(orient='records'), ensure_ascii=False) + ';\n')
+    with open('output/globe.js', 'w', encoding='utf-8') as f:
+        f.write('var food_waste =\n' + json.dumps(
+            df.to_dict(orient='records'),
+            ensure_ascii=False) + ';\n')
+
+        f.write('var undernourishment =\n' + json.dumps(
+            un.drop(columns='country').set_index('iso3').to_dict(orient='index'),
+            ensure_ascii=False) + ';\n')
